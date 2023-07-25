2e62f62a7914bf7591b08c679d3f44a99812604e
Ivan Panaitov <PanaitovIV@gmail.com>
2023-07-25 14:22:34 +0300

    добавил main

---
+++ src/family_tree/Main.java
@@ -0,0 +1,10 @@
+package family_tree;
+
+import family_tree.view.Console;
+
+public class Main {
+    public static void main(String[] args) {
+        Console consoleUI = new Console();
+        consoleUI.start();
+    }
+}
